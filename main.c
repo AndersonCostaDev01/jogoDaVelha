9ebff45df94318d8c8fe4d248799bca29237baaa
Anderson Costa <costaandersom23@gmail.com>
2026-05-28 09:41:57 -0300

    Refactor game logic and board initialization for improved functionality and clarity

--- main.c
+++ main.c
@@ -1,43 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-#define LINHA 3
-#define COLUNA 3
+#define SIZE 3
 
+// Função para inicializar o tabuleiro
+void initializeBoard(char board[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            board[i][j] = ' ';
+        }
+    }
+}
 
-int tabuleiro[3][3];
-
-int desenhaTabuleiro(){
-    for(int l = 0; l < LINHA; l++){
-        for(int c = 0; c < COLUNA; c++){
-            tabuleiro[l][c] = 0;
-            printf("%d ", tabuleiro[l][c]);
+// Função para desenhar o tabuleiro
+void drawBoard(char board[SIZE][SIZE]) {
+    system("clear"); // Limpa a tela no Linux/Mac, use "cls" em Windows
+    for (int i = 0; i < SIZE; i++) {
+        printf("|");
+        for (int j = 0; j < SIZE; j++) {
+            printf("%c|", board[i][j]);
         }
         printf("\n");
+        if (i < SIZE - 1) {
+            printf("-----\n");
+        }
     }
-    return 0;
 }
 
-int random(){
-    int aleatorio[2];
-    aleatorio[0] = rand() % 3;
-    aleatorio[1] = rand() % 3;
-    for(int i = 0; i < 2; i++){
-        printf("%d ", aleatorio[i]);
+// Função para verificar se alguém ganhou
+char checkWin(char board[SIZE][SIZE]) {
+    // Verifica linhas e colunas
+    for (int i = 0; i < SIZE; i++) {
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
+            return board[i][0];
+        }
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ') {
+            return board[0][i];
+        }
     }
-    printf("\n");
-    return 0;
+
+    // Verifica diagonais
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
+        return board[0][0];
+    }
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
+        return board[0][2];
+    }
+
+    // Verifica se há empate
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (board[i][j] == ' ') {
+                return ' '; // Ainda há espaços vazios, jogo não acabou
+            }
+        }
+    }
+
+    return 'D'; // Empate
 }
 
+// Função principal do jogo
 int main() {
-    srand(time(NULL));
-    desenhaTabuleiro();
-    int win = 0;
-    for(int i = 0; i < 5; i++){
-        random();
+    char board[SIZE][SIZE];
+    initializeBoard(board);
+    int currentPlayer = 1; // 1 para jogador X, -1 para jogador O
+    int row, col;
+    char winner;
+
+    while (1) {
+        drawBoard(board);
+
+        if (currentPlayer == 1) {
+            printf("Jogador X, digite a linha (0-2): ");
+            scanf("%d", &row);
+            printf("Jogador X, digite a coluna (0-2): ");
+            scanf("%d", &col);
+        } else {
+            printf("Jogador O, digite a linha (0-2): ");
+            scanf("%d", &row);
+            printf("Jogador O, digite a coluna (0-2): ");
+            scanf("%d", &col);
+        }
+
+        // Verifica se a posição está vazia
+        if (board[row][col] == ' ') {
+            board[row][col] = currentPlayer == 1 ? 'X' : 'O';
+        } else {
+            printf("Posição já ocupada, tente novamente.\n");
+            continue;
+        }
+
+        // Verifica se alguém ganhou ou se houve empate
+        winner = checkWin(board);
+        if (winner != ' ') {
+            drawBoard(board);
+            if (winner == 'D') {
+                printf("Empate!\n");
+            } else {
+                printf("Jogador %c venceu!\n", winner);
+            }
+            break;
+        }
+
+        // Alterna entre jogadores
+        currentPlayer = -currentPlayer;
     }
-    
 
     return 0;
 }
